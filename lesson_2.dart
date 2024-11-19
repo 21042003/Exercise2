1d5f7ffbeaa9a8cb39e0b1b22e3a84abb5244c39
haipham <haiphamnhu203@gmail.com>
2024-11-19 10:07:00 +0700

    suabt2

--- lesson_2.dart
+++ lesson_2.dart
@@ -1,24 +1,28 @@
 import 'dart:math';
 
 void main() {
-  double a = 3;
+  double a = 0;
   double b = 1;
   double c = 2;
 
-  double delta = pow(b, 2) - 4 * a * c;
-  print("Delta = $delta");
-
-  if (delta > 0) {
-    double x1 = (-b + sqrt(delta)) / (2 * a);
-    double x2 = (-b - sqrt(delta)) / (2 * a);
-    print("Phuong trinh co hai nghiem phan biet:");
-    print("x1 = $x1");
-    print("x2 = $x2");
-  } else if (delta == 0) {
-    double x = -b / (2 * a);
-    print("Phuong trinh co nghiem kep:");
-    print("x = $x");
+  //kiem tra xem có phải phương trình bậc hai không
+  if (a == 0) {
+    print("Không phải phương trình bậc hai");
   } else {
-    print("Phuong trinh vo nghiem.");
+    double delta = pow(b, 2) - 4 * a * c;
+    print("Delta = $delta");
+    if (delta > 0) {
+      double x1 = (-b + sqrt(delta)) / (2 * a);
+      double x2 = (-b - sqrt(delta)) / (2 * a);
+      print("Phuong trinh co hai nghiem phan biet:");
+      print("x1 = $x1");
+      print("x2 = $x2");
+    } else if (delta == 0) {
+      double x = -b / (2 * a);
+      print("Phuong trinh co nghiem kep:");
+      print("x = $x");
+    } else {
+      print("Phuong trinh vo nghiem.");
+    }
   }
 }
